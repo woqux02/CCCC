c8a46d25922969d324d60b01577354fe7202e553
珩 张 <zhangheng8699@outlook.com>
2023-10-05 21:04:26 +0800

    10.5_Chapter6_unfinished

---
+++ Chapter_6/Chapter6solu/PeopleApp/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using Packt.Shared;
+using static System.Console;
+
+namespace Chapter6
+{
+    public class Program
+    {
+        private static void Harry_Shout(object sender, EventArgs e)
+        {
+            Person p = (Person)sender;
+            WriteLine($"{p.Name} is this angry: {p.AngerLevel}");
+        }
+
+        public static void Main(string[] args)
+        {
+            var harry = new Person { Name = "Harry" };
+            var mary = new Person { Name = "Mary" };
+            var jill = new Person { Name = "Jill" };
+
+            // 调用实例方法
+            var baby1 = mary.SonWith(harry);
+            baby1.Name = "Gary";
+
+            // 调用静态方法
+            var baby2 = Person.Sons(jill, mary);
+
+            WriteLine($"{harry.Name} and {mary.Name} have a son {baby1.Name}");
+            WriteLine($"{jill.Name} and {mary.Name} have a son {baby2.Name}");
+            WriteLine($"mary has {mary.Children.Count} son");
+
+            // 调用运算符（已经被重载）
+            var baby3 = harry * mary;
+            WriteLine($"{harry.Name} has {harry.Children.Count} sons.");
+
+            // 调用 Harry_Shout 方法，不知道为什么报错，但是可以执行
+            harry.Shout = Harry_Shout;
+            harry.Poke();
+            harry.Poke();
+            harry.Poke();
+            harry.Poke();
+        }
+
+
+    }
+}
---
+++ Chapter_6/Chapter6solu/Person/Person.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using static System.Console;
+
+namespace Packt.Shared
+{
+    public class Person
+    {
+        // 有字段 Name，DateOfBirth，Person类的List叫Children
+        public string Name;
+        public DateTime DateOfBirth;
+        public List<Person> Children = new List<Person>();
+
+        public void WriteToConsole()
+        {
+            WriteLine($"{Name} was born on a {DateOfBirth:dddd}.");
+        }
+
+        // 静态方法 （里面 new Person 后为什么用 {} ？）
+        public static Person Sons(Person p1, Person p2)
+        {
+            var baby = new Person
+            {
+                Name = $"Baby of {p1.Name} and {p2.Name}."
+            };
+
+            p1.Children.Add(baby);
+            p2.Children.Add(baby);
+
+            return baby;
+        }
+
+        // 实例方法
+        public Person SonWith(Person partner)
+        {
+            return Sons(this, partner);
+        }
+
+        // 重载运算符，这里operator是字段名，不是关键字
+        public static Person operator *(Person p1, Person p2)
+        {
+            return Person.Sons(p1, p2);
+        }
+
+        // 一个很普通的方法
+        public int NameLength(string s)
+        {
+            return s.Length;
+        }
+
+        // 创建一个委托，这个案例不知道怎么写
+        // delegate int ToMethod(string m);
+        // var d = new ToMethod(p1.NameLength);
+
+        public EventHandler Shout;
+        public int AngerLevel;
+        public void Poke()
+        {
+            AngerLevel++;
+            if(AngerLevel >= 3)
+            {
+                if(Shout != null)
+                {
+                    Shout(this, EventArgs.Empty);
+                }
+            }
+        }
+    }
+}
